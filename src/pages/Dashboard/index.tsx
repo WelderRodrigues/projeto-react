a00c5ed9faf87e03326b387866b8e4483978c308
Welder Rodrigues <welder_15@hotmail.com>
2020-06-24 23:20:30 -0300

    Estilizando dashboard
    Criada a tela de dashboard de forma estatica fiel ao layout da aplicação

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import githubLogo from '../../assets/githubLogo.svg';
+
+import { Title, Form, Repositories } from './styles';
 const Dashboard: React.FC = () => {
-    return <Title>Explore repositórios no Github</Title>;
+    return (
+        <>
+            <img src={githubLogo} alt="Github Explorer" />
+            <Title>Explore repositórios no Github</Title>;
+            <Form>
+                <input placeholder="Digite o nome do repositório" type="text" />
+                <button type="submit">Pesquisar</button>
+            </Form>
+            <Repositories>
+                <a href="#">
+                    <img
+                        src="https://avatars2.githubusercontent.com/u/11357244?s=460&u=c232d975a857dbbef4048695540ecd22400b9a75&v=4"
+                        alt="Welder Rodrigues"
+                    />
+                    <div>
+                        <strong>NLW</strong>
+                        <p>Ecoleta - Next Level Week #01 </p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories>
+        </>
+    );
 };
 
 export default Dashboard;
