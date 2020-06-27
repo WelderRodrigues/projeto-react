8ca0b19e8e5a8f37f7fb3c8df9a030489078c921
Welder Rodrigues <welder_15@hotmail.com>
2020-06-26 22:26:45 -0300

    Estilizando detalhes
    Criada a parte html e css dos detalhes dos repositorios

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import githubLogo from '../../assets/githubLogo.svg';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
+import { Header, RepositoryInfo, Issues } from './styles';
 interface RepositoryParams {
     repository: string;
 }
@@ -8,7 +11,43 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
     const { params } = useRouteMatch<RepositoryParams>();
 
-    return <h1> Repository:{params.repository} </h1>;
+    return (
+        <>
+            <Header>
+                <img src={githubLogo} alt="Github Explorer" />
+                <Link to="/">
+                    <FiChevronLeft size={20} />
+                    Voltar
+                </Link>
+            </Header>
+
+            <RepositoryInfo>
+                <header>
+                    <img src="" alt="" />
+                    <div>
+                        <strong></strong>
+                        <p></p>
+                    </div>
+                </header>
+                <ul>
+                    <li>
+                        <strong></strong>
+                        <span></span>
+                    </li>
+                </ul>
+            </RepositoryInfo>
+
+            <Issues>
+                <Link to="aaa">
+                    <div>
+                        <strong>aaa</strong>
+                        <p>aaa</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </Link>
+            </Issues>
+        </>
+    );
 };
 
 export default Repository;
